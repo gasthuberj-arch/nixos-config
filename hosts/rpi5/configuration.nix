e5de1dda53296ec1463163ea97f86238cf3de408
devhuber <johannes.gasthuber@gmx.de>
2026-08-26 13:30:18 +0200

    Update rpi5 config to import core base and remove deprecated bootloader option

--- hosts/rpi5/configuration.nix
+++ hosts/rpi5/configuration.nix
@@ -1,10 +1,9 @@
-{
-  config,
-  lib,
-  nixos-raspberrypi,
-  ...
-}: {
+{nixos-raspberrypi, ...}: {
   imports = [
+    # Universal baseline
+    ../../modules/core/base.nix
+
+    # Raspberry Pi 5 kernel & hardware modules
     nixos-raspberrypi.nixosModules.sd-image
     nixos-raspberrypi.nixosModules.raspberry-pi-5.base
     nixos-raspberrypi.nixosModules.raspberry-pi-5.page-size-16k
@@ -13,26 +12,7 @@
   networking.hostName = "rpi5";
   networking.networkmanager.enable = true;
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-
-  boot.loader.raspberryPi = {
-    enable = true;
-    # version = 5;
-    bootloader = "kernel";
-  };
-
   hardware.enableRedistributableFirmware = true;
-
-  services.openssh.enable = true;
-
-  users.users.johannes = {
-    isNormalUser = true;
-    extraGroups = ["wheel" "networkmanager"];
-    # Add SSH keys or a password before first boot.
-  };
-
-  time.timeZone = "Europe/Berlin";
-  i18n.defaultLocale = "en_US.UTF-8";
 
   system.stateVersion = "25.11";
 }
